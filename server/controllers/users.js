46d077fbf31943462ffb65ecd199777e7f4e3d3e
Cameron <cameron.e.pardo@gmail.com>
2019-02-28 10:51:06 -0500

    error in users

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -11,6 +11,14 @@ app.get("/",(eq,res)=>{     //every action has a method and a path / is the root
         res.send(data);
 
     });
+app.get("/:id",(eq,res)=>{     //every action has a method and a path / is the root of this controller
+    user.get(req.params.id,(err, data) => {
+        console.log('violets are blue')
+        if(err) throw err;  // once function is called,call send method
+        res.send(data);
+
+});
+   
     console.log('aysnc is great')
 });
 
--- server/models/users.js
+++ server/models/users.js
@@ -2,18 +2,33 @@ const conn = require('./mysql_connections');
 
 const model = {
     getAll(cb){
-        conn.query("select * from 2019Spring_Persons", (err, data)=> {
+        conn.query("SELECT * FROM 2019Spring_Persons", (err, data) => {
             cb(err, data);
-        });
+        });    
     },
     get(id, cb){
 
     },
-    
-    add(input){
-
+    add(input, cb){
+        // sql server cannot do this kind of validation.
+        if(input.Password.length < 8){
+            cb(Error('A longer password is required'));
+            // do not want function to play out so return
+            return;
+        }
+        conn.query( "INSERT INTO 2019Spring_Persons (FirstName,LastName,Birthday,Password,created_at) VALUES (?)",
+                    [[input.FirstName, input.LastName, input.Birthday, input.Password, new Date()]],
+                    (err, data) => {
+                        if(err){
+                            cb(err);
+                            return;
+                        }
+                        model.get(data.insertId, (err,data) =>{
+                            cb(err, data); 
+                        })
+                    }
+        );    
     }
-
 };
 
 module.exports = model;
